3c73722400b8da64349c11bb0deb79aa1ad115ef
Kayla Gentry <kaylagentry@kaylas-mbp.mynetworksettings.com>
2025-02-04 18:35:06 -0500

    updated css file

--- index.js
+++ index.js
@@ -2,17 +2,27 @@
 
 //Todo: set the correct number wether its random or set
 // correct number is 37
-let correctNumber = 37;
-// let correctNumber = Math.floor(Math.random() * 100) + 1;
-let attempts = 7
+// let correctNumber = 37;
+let correctNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 7; //user get 7 atteps total
+let guesses = []; //aray to store past guesses
 
 //Todo: Ask the user to choose a number between 1-100
 let userGuess = Number(prompt("Guess a number between 1 & 100:"));
+
+//Todo: Get the ul element to display guesses
+let guessList = document.getElementById('guessList');
 
 //Todo: create a while loop
 
 while (attempts > 0) {
     let userGuess = Number(prompt(`Guess a number between 1 and 100. You have ${attempts} attempts left`))
+
+    guesses.push(userGuess);// add guess to the guesses array
+
+    let li = document.createElement('Li');
+    li.textContent = `Attempt ${7 - attempts}: ${userGuess}`; //Show the attempt and number of guesses
+    guessList.appendChild(li); //add the <li> to the list on the page
 
     if (userGuess === correctNumber) {
         alert("Congradulations you guessed the number! ");
